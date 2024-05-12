f4d7af82922694b84a57f00a42f6dbae5377fef0
FranMeneses <francisco.meneses.villegas@gmail.com>
2024-05-12 08:11:21 -0400

    Features: established database conection by typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
@@ -12,8 +15,14 @@ import { AppService } from './app.service';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: [],
       synchronize: true,
+      ssl: process.env.DB_SSL === 'true',
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
     }),
   ],
   controllers: [AppController],
