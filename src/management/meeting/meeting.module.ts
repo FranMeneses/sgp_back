8986047ca7d38e6176ba4db589d25f55ef775d7b
FranMeneses <francisco.meneses.villegas@gmail.com>
2024-06-08 11:11:34 -0400

    Feature: missing module elements added

--- src/management/meeting/meeting.module.ts
+++ src/management/meeting/meeting.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MeetingService } from './meeting.service';
+import { MeetingController } from './meeting.controller';
 
-@Module({})
+@Module({
+  providers: [MeetingService],
+  controllers: [MeetingController]
+})
 export class MeetingModule {}
--- src/management/resource/resource.module.ts
+++ src/management/resource/resource.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ResourceService } from './resource.service';
+import { ResourceController } from './resource.controller';
 
-@Module({})
+@Module({
+  providers: [ResourceService],
+  controllers: [ResourceController]
+})
 export class ResourceModule {}
